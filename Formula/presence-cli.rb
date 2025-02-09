186a5e2a04347d988f776ecbf156a01ea4b56f1d
Lewis Westbury <lewis@cantab.net>
2025-02-09 14:41:08 +0000

    Update to version 0.3.3

--- Formula/presence-cli.rb
+++ Formula/presence-cli.rb
@@ -1,12 +1,12 @@
 class PresenceCli < Formula
     desc "Tools for formatting and posting threads to social networks."
     homepage "https://instantiator.dev/presence"
-    url "https://github.com/instantiator/presence/releases/download/0.3.2/Presence.zip"
-    version "0.3.2"
+    url "https://github.com/instantiator/presence/releases/download/0.3.3/Presence.zip"
+    version "0.3.3"
     license "MIT"
     
     # get sha: shasum -a 256 Presence.zip
-    sha256 "e5a434da12ca9a4af35068899ea0b56127d95f3510cb186ed7ea2ffb0ec216c0"
+    sha256 "b88afa5ec1dfaed39e9067ae04210e774f725659cdb3a5a91f931db3d8923e30"
 
     def install
         # linux
